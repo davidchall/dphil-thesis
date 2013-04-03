e2ccd2fa68c51c030e338b2ab525b7dba6583112
David Hall <dhcrawley@gmail.com>
2013-04-03 19:01:09 +0100

    Update hooks

--- macros/pre-commit.sh
+++ macros/pre-commit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "Testing pre-commit hook"
-exit 1
+./macros/texcount.pl -opt=macros/texcount_options.txt thesis.tex > macros/texcount_output.txt
+./macros/drawCharts.py
+git add macros/texcount_output.txt
+git add macros/wordcount.txt
+git add macros/stats.html
+
